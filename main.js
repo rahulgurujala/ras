edbe8f85d9ec59610f47a2309e04cc3589ca3f05
rahulgurujala <isaacnewtonrahul@gmail.com>
2020-04-25 23:58:03 +0530

    Initialization

---
+++ main.js
@@ -0,0 +1,28 @@
+let fInpt = document.getElementById('fInput');
+let sInpt = document.getElementById('sInput');
+let tInpt = document.getElementById('tInput');
+let subBtn = document.getElementById('subBtn');
+let answer = document.getElementById('ans');
+let clearBtn = document.getElementById('cls');
+let ans;
+
+function clear(e) {
+  e.preventDefault();
+  fInpt.value = '';
+  sInpt.value = '';
+  tInpt.value = '';
+}
+
+function calculate(e) {
+  e.preventDefault();
+  a = Number(fInpt.value);
+  b = Number(sInpt.value);
+  c = Number(tInpt.value);
+  ans = `${(a * b)}${(a * c)}${a * (b + c) - b}`;
+  console.log(ans);
+  answer.style.display = 'block';
+  answer.innerText = `${a} + ${b} + ${c} = ${ans}`;
+}
+
+subBtn.addEventListener('click', calculate);
+clearBtn.addEventListener('click', clear);
